858dab75a0131f90b3af71198053c0260917b3d3
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2023-10-19 10:25:43 +0530

    signup auth created

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -17,11 +18,8 @@ app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
 
-app.get('/test', (request,response) => {
-    response.json({
-        message: "hello world",
-    });
-})
+app.use(express.json());
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
