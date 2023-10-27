4b09b457b117d1395a822e8d94fee0a1b430a4ae
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2023-10-27 11:47:36 +0530

    adding google OAuth

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import OAuth from '../components/OAuth';
 
 export default function SignUp() {
 
@@ -52,7 +53,7 @@ export default function SignUp() {
         <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
         <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
         <button disabled={loading} className='bg-black text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80'>{loading? 'Loading...':'Sign Up'}</button>
-        <button className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80'>Continue with Google</button>
+        <OAuth/>
       </form>
       <div className='flex gap-2 mt-3'>
         <p>Have an account?</p>
