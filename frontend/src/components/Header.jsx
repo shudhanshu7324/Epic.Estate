752acddb0d942c964173beb1fa04726e58a0359d
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2023-10-28 23:47:21 +0530

    updating the header and making the profile page private

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import {FaSearch} from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function Header() {
+
+  const {currentUser} = useSelector(state => state.user);
+
   return (
     <header className='bg-black shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -26,8 +30,8 @@ export default function Header() {
             <Link to='/about'>
             <li className='hidden sm:inline text-gray-200 hover:underline'>About</li>
             </Link>
-            <Link to='/sign-in'>
-            <li className='text-gray-200 hover:underline'>Sign In</li>
+            <Link to='/profile'>
+            { currentUser? (<img src={currentUser.avatar} alt='profile' className='rounded-full h-7 w-7 object-cover' />) : (<li className='text-gray-200 hover:underline'>Sign In</li>) }
             </Link>
         </ul>
 
