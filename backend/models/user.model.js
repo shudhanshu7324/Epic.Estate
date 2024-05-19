4d863388b5a021e134c8db90e11265d49df46ca6
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-05-19 21:03:37 +0530

    delete user function completed

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
     },
     avatar: {
         type: String,
-        default: "https://images.pexels.com/photos/23657500/pexels-photo-23657500/free-photo-of-a-man-in-black-shirt-standing-in-front-of-a-light.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        default: ""
     },
 }, {timestamps: true});
 
--- frontend/src/redux/user/userSlice.js
+++ frontend/src/redux/user/userSlice.js
@@ -33,9 +33,23 @@ const userSlice = createSlice({
         updateUserFailure: (state,action) => {
             state.error = action.payload;
             state.loading = false;
+        },
+        deleteUserStart: (state) => {
+            state.loading = true;
+        },
+        deleteUserSuccess: (state) => {
+            state.currentUser = null;
+            state.loading = false;
+            state.error = null;
+        },
+        deleteUserFailure: (state) => {
+            state.error = action.payload;
+            state.loading = false
         }
     }
 });
 
-export const { signInStart, signInFailure, signInSuccess, updateUserStart, updateUserSuccess, updateUserFailure} = userSlice.actions;
+export const { signInStart, signInFailure, signInSuccess, updateUserStart, updateUserSuccess, updateUserFailure,
+                deleteUserStart, deleteUserSuccess, deleteUserFailure
+} = userSlice.actions;
 export default userSlice.reducer;
